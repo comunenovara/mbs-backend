69845fb649cee3e349d9243ab17edfaa34d55b7c
Cavallaro Catello Stefano <stefano@stepo.biz>
2022-11-29 11:13:26 +0100

    readme

--- src/mbs/mbs.module.ts
+++ src/mbs/mbs.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AssetBusinessService } from './business/asset.business.service';
 import { PrismaService } from './repository/prisma.service';
+import { AssetBusinessService } from './business/asset.business.service';
 import { AssetController } from './web/rest/asset.controller';
 
 @Module({
